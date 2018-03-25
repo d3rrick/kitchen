2f525899c5d62062cf526f3c9e3370bf22285891
muriithiderro <muriithiderrick56@gmail.com>
2018-03-25 18:19:20 +0300

    added google analytics

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+
+
+
+
+  $(".breakfast h1").mouseover(function(){
+    $(this).css("color", "black");
+    $(".breakfast").css( "opacity",0.4);
+
+  });
+  $(".lunch h1").mouseover(function(){
+    $(this).css("color", "black");
+    $(".lunch").css( "opacity",0.4);
+
+  });
+  $(".dinner h1").mouseover(function(){
+    $(this).css("color", "black");
+    $(".dinner").css( "opacity",0.4);
+
+  });
+
+   $(".menu").click(function(){
+     alert("hi");
+   });
+
+   $(".embtn").click(function(){
+     var email = $("#email").val();
+     alert( "hello visitor, you have successifully been subscribed.. " +email);
+
+   });
+
+});
